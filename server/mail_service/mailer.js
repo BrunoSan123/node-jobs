712b814e9788869e5646a6156d4de050c2a14ec0
Bruno Barreto <Bruno-kk@hotmail.com>
2022-11-10 15:03:32 -0300

    fix: email msg fixed

--- server/mail_service/mailer.js
+++ server/mail_service/mailer.js
@@ -16,9 +16,9 @@ const sendMailer=(erro)=>{
         from: 'bruno-kk@hotmail.com',
         to: '3cc329baab-1f6ae8@inbox.mailtrap.io',
         subject: 'Dominio em Lentidão',
-        text: `Os seguintes dominção estão com muita lantência:\n
-               Host:${erro.hostname}\n
-               Erro:${erro.errno,erro.code,erro.config.timeout}
+        text: `Os seguintes dominção estão com muita lantência:
+               Host:${erro.config.url}\n
+               Erro:{Numero:${erro.errno} Código:${erro.code} Tempo:${erro.config.timeout}}
         `
     },(err,data)=>{
         if(err){
